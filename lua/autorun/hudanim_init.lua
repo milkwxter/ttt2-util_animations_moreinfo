ccadd0a3b8b910a3c04b66b6aa17195707f21e89
milkwxter <ethan.r.bates2003@gmail.com>
2026-09-18 17:17:17 -0500

    removed thing i dont use

--- lua/autorun/hudanim_init.lua
+++ lua/autorun/hudanim_init.lua
@@ -4,9 +4,7 @@ if SERVER then
 	AddCSLuaFile()
 	AddCSLuaFile("animations/client/calculations/cubic_bezier.lua")
 	AddCSLuaFile("animations/client/role_anim.lua")
-	AddCSLuaFile("animations/client/connect_anim.lua")
 else
 	include("animations/client/calculations/cubic_bezier.lua")
 	include("animations/client/role_anim.lua")
-	include("animations/client/connect_anim.lua")
 end
